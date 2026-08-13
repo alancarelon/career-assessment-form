916438e244f0d2aed03ec10fd1206a8851567209
alancarelon <alankrita.goutham@carelon.com>
2026-08-13 14:21:36 +0530

    Fix double BrowserRouter issue - move all routes to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UXGrowthJourney from './components/UXGrowthJourney'
-import AdminDashboard from './pages/AdminDashboard'
-import UserReport from './pages/UserReport'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<UXGrowthJourney />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admin/user/:id" element={<UserReport />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <UXGrowthJourney />
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
 import AdminDashboard from './pages/AdminDashboard.tsx'
+import UserReport from './pages/UserReport.tsx'
 import TestConnection from './pages/TestConnection.tsx'
 import './index.css'
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/user/:id" element={<UserReport />} />
         <Route path="/test" element={<TestConnection />} />
       </Routes>
     </BrowserRouter>
